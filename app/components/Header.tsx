6220e7318ea24672d2e94857830729587eaf85e3
Jerome <nietsemorej@gmail.com>
2024-11-17 00:21:31 -0800

    add light/dark mode

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,12 +1,16 @@
 import Link from 'next/link'
+import DarkModeToggle from '@/app/components/dark-mode-toggle'
+import useServerDarkMode from '@/app/hooks/use-server-dark-mode'
 
 export default function Header({ className }: { className?: string }) {
+  const theme = useServerDarkMode()
+
   return (
     <header className={`flex justify-between items-center ${className}`}>
       <Link href="/dashboard" className="text-xl hover:underline underline-offset-8 decoration-2">Finance App</Link>
 
       <div className="flex items-center space-x-4">
-        <div>Mode Toggle</div>
+        <DarkModeToggle defaultMode={theme === 'dark' ? 'dark' : 'light'} />
         <div>User Dropdown</div>
       </div>
     </header>
---
+++ app/hooks/use-server-dark-mode.tsx
@@ -0,0 +1,5 @@
+import { cookies } from 'next/headers'
+const useServerDarkMode = async (defaultTheme = 'dark') => {
+  return (await cookies()).get('theme')?.value ?? defaultTheme
+}
+export default useServerDarkMode
