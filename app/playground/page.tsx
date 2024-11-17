a26d0d2086a32996406a8706176505bab3530d0d
Jerome <nietsemorej@gmail.com>
2024-11-16 18:49:47 -0800

    add transaction component

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,5 +1,6 @@
 import Header from '@/app/components/Header'
 import Trend from '@/app/components/Trend'
+import TransactionItem from '@/app/components/TransactionItem'
 
 export default function Playground() {
   return (
@@ -19,6 +20,17 @@ export default function Playground() {
           <Trend type="Expense" amount={1000} prevAmount={900} />
           <Trend type="Savings" amount={1000} prevAmount={1100} />
           <Trend type="Investments" amount={1000} prevAmount={1100} />
+        </div>
+      </div>
+
+      <div>
+        <h2 className="mb-4 text-lg font-mono">TransactionItem</h2>
+        <hr className="mb-4 border-gray-200 dark:border-gray-800" />
+        <div className="space-y-4">
+          <TransactionItem type="Income" description="Salary" amount={2000} />
+          <TransactionItem type="Expense" category="Food" description="Going out to eat" amount={29} />
+          <TransactionItem type="Saving" description="For children" amount={500} />
+          <TransactionItem type="Investment" description="In Microsoft" amount={9000} />
         </div>
       </div>
     </main>
