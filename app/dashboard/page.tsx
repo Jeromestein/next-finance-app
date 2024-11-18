902b704d107e9e3dab809487dfe4534d027d0492
Jerome <nietsemorej@gmail.com>
2024-11-18 08:58:23 -0800

    Add transaction page & link

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,11 +3,16 @@ import TransactionListFallback from '@/app/components/transaction-list-fallback'
 import Trend from './components/trend'
 import TrendFallback from './components/trend-fallback'
 import { Suspense } from 'react'
+import Link from 'next/link'
+import { PlusCircle } from 'lucide-react'
+import { sizes, variants } from "@/lib/variants"
 
 export default function Dashboard() {
   return (
     <main>
-      <h1>Dashboard</h1>
+      <section className="mb-8">
+        <h1 className="text-4xl font-semibold">Summary</h1>
+      </section>
       <section className="mb-8 grid grid-cols-2 lg:grid-cols-4 gap-8">
         <Suspense fallback={<TrendFallback />}>
           <Trend type="Income" />
@@ -21,6 +26,14 @@ export default function Dashboard() {
         <Suspense fallback={<TrendFallback />}  >
           <Trend type="Investment" />
         </Suspense>
+      </section>
+
+      <section className="flex justify-between items-center mb-8">
+        <h2 className="text-2xl">Transactions</h2>
+        <Link href="/dashboard/transaction/add" className={`flex items-center space-x-1 ${variants['outline']} ${sizes['sm']}`}>
+          <PlusCircle className="w-4 h-4" />
+          <div>Add</div>
+        </Link>
       </section>
       <Suspense fallback={<TransactionListFallback />}>
         <TransactionList />
