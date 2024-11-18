7a8ab9f00b49249634195ceeebdcd10bc808b6c9
Jerome <nietsemorej@gmail.com>
2024-11-18 09:22:57 -0800

    add react hook form

--- app/dashboard/components/transaction-form.tsx
+++ app/dashboard/components/transaction-form.tsx
@@ -1,39 +1,51 @@
+'use client'
+
 import Button from "@/app/components/button";
 import Input from "@/app/components/input";
 import Label from "@/app/components/label";
 import Select from "@/app/components/select";
 import { categories, types } from "@/lib/consts";
+import { useForm } from "react-hook-form";
 
 export default function TransactionForm() {
-  return <form className="space-y-4">
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
+
+  const onSubmit = (data: any) => console.log(data)
+
+  return <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
     <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
       <div>
         <Label className="mb-1">Type</Label>
-        <Select>
+        <Select {...register('type')}>
           {types.map(type => <option key={type}>{type}</option>)}
         </Select>
       </div>
 
       <div>
         <Label className="mb-1">Category</Label>
-        <Select>
+        <Select {...register('category')}>
           {categories.map(category => <option key={category}>{category}</option>)}
         </Select>
       </div>
 
       <div>
         <Label className="mb-1">Date</Label>
-        <Input />
+        <Input {...register('created_at')} />
       </div>
 
       <div>
         <Label className="mb-1">Amount</Label>
-        <Input type="number" />
+        <Input type="number" {...register('amount')} />
       </div>
 
       <div className="col-span-2">
         <Label className="mb-1">Description</Label>
-        <Input type="number" />
+        <Input {...register('description')} />
       </div>
     </div>
 
