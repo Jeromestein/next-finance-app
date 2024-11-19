e37ba74519ac3b826720fe7dc9cd0aa37c1a6ea0
Jerome <nietsemorej@gmail.com>
2024-11-18 17:18:06 -0800

    add erroe boundary for trend and remove json server

--- app/dashboard/components/trend.tsx
+++ app/dashboard/components/trend.tsx
@@ -1,4 +1,5 @@
 import BaseTrend from "@/app/components/trend"
+
 export default async function Trend({ type }: { type: string }) {
   const response = await fetch(`${process.env.API_URL}/trends/${type}`)
   const { amount, prevAmount } = await response.json()
