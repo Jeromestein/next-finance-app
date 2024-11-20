1d8c6d17353320e52092ef3e7bfadc21ff6a5833
Jerome <nietsemorej@gmail.com>
2024-11-19 16:13:04 -0800

    add form pending state

--- app/(auth)/login/components/login-form.tsx
+++ app/(auth)/login/components/login-form.tsx
@@ -1,4 +1,6 @@
-import Button from "@/app/components/button";
+"use client"
+
+import SubmitButton from "@/app/components/submit-button";
 import Input from "@/app/components/input";
 import { login } from "@/lib/actions";
 
@@ -6,6 +8,8 @@ export default function LoginForm() {
   return <form action={login} className="space-y-2">
     <Input type="email" placeholder="name@example.com"
       name="email" required />
-    <Button type="submit" size="sm" className="w-full">Sign in with email</Button>
+    <SubmitButton size="sm" className="w-full">
+      Sign in with email
+    </SubmitButton>
   </form>
 }
